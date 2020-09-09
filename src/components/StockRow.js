c05047706d0b67e4ff02920b33e7522d22af8f0d
allezz <aerbay@gmail.com>
2020-09-09 17:04:54 +0200

    Add axios and remove fetch

--- src/components/StockRow.js
+++ src/components/StockRow.js
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { iex } from "../config/iex.js";
+import axios from "axios";
 
 const StockRow = (props) => {
   const [finData, setFinData] = useState({});
 
-  useEffect(() => {
+  useEffect(async () => {
     const url = `${iex.base_url}/stock/${props.ticker}/intraday-prices?chartLast=1&token=${iex.api_token}`;
-    fetch(url)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setFinData(data[data.length - 1]);
-      });
+    // fetch(url)
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     setFinData(data[data.length - 1]);
+    //   });
+    try {
+      let response = await axios.get(url);
+      setFinData(response.data[response.data.length - 1]);
+    } catch (err) {
+      console.log(err);
+    }
   }, []);
 
   return (
