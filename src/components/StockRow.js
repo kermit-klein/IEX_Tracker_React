181e0df523fd915263f28ccd3d29e669a1828d34
allezz <aerbay@gmail.com>
2020-09-09 16:59:20 +0200

    Add config iex file, Useffect fetch

--- src/components/StockRow.js
+++ src/components/StockRow.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { iex } from "../config/iex.js";
 
 const StockRow = (props) => {
+  const [finData, setFinData] = useState({});
+
+  useEffect(() => {
+    const url = `${iex.base_url}/stock/${props.ticker}/intraday-prices?chartLast=1&token=${iex.api_token}`;
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setFinData(data[data.length - 1]);
+      });
+  }, []);
+
   return (
     <tr>
       <td>{props.ticker}</td>
-      <td>dfsdfsdf</td>
-      <td>dfsdfsdf</td>
-      <td>dfsdfsdf</td>
+      <td>{finData.price}</td>
+      <td>{finData.date}</td>
+      <td>{finData.time}</td>
     </tr>
   );
 };
---
+++ src/config/iex.js
@@ -0,0 +1,4 @@
+export const iex = {
+  api_token: "pk_e83231cddd014a5fbea360cc68bf8134",
+  base_url: "https://cloud.iexapis.com/stable",
+};
