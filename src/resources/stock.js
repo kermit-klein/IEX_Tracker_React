90a00271570e4ed955442276a05317e4cbc18679
allezz <aerbay@gmail.com>
2020-09-09 19:47:39 +0200

    Refactor stock.js

--- src/resources/stock.js
+++ src/resources/stock.js
@@ -3,8 +3,20 @@ import axios from "axios";
 
 export const stock = {
   latestPrice: async (ticker) => {
-    const url = `${iex.base_url}/stock/${ticker}/intraday-prices?chartLast=1&token=${iex.api_token}`;
+    const url = stock.latestPriceUrl(ticker);
     const query = await axios.get(url);
-    return query;
+    return stock.formatPriceData(query);
+  },
+  latestPriceUrl: (ticker) => {
+    return `${iex.base_url}/stock/${ticker}/intraday-prices?chartLast=1&token=${iex.api_token}`;
+  },
+  formatPriceData: (data) => {
+    let stockData = data.data[data.data.length - 1];
+    let formattedData = {
+      price: stockData.close,
+      date: stockData.date,
+      time: stockData.label,
+    };
+    return formattedData;
   },
 };
