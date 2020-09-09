8bf14fbdca354c855e471b95ae22a07ce99d96d4
allezz <aerbay@gmail.com>
2020-09-09 18:26:08 +0200

    Move latest price algo to resources, begin filtering results in stock.js

--- src/components/StockRow.js
+++ src/components/StockRow.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { iex } from "../config/iex.js";
 import axios from "axios";
+import { stock } from "../resources/stock.js";
 
 const StockRow = (props) => {
   const [finData, setFinData] = useState({});
+
+  const applyData = (data) => {
+    setFinData(data);
+  };
 
   useEffect(async () => {
     const url = `${iex.base_url}/stock/${props.ticker}/intraday-prices?chartLast=1&token=${iex.api_token}`;
@@ -14,20 +19,22 @@ const StockRow = (props) => {
     //   .then((data) => {
     //     setFinData(data[data.length - 1]);
     //   });
-    try {
-      let response = await axios.get(url);
-      setFinData(response.data[response.data.length - 1]);
-    } catch (err) {
-      console.log(err);
-    }
+    let data = await stock.latestPrice(props.ticker);
+    applyData(data);
+    // try {
+    //   let response = await axios.get(url);
+    //   setFinData(response.data[response.data.length - 1]);
+    // } catch (err) {
+    //   console.log(err);
+    // }
   }, []);
 
   return (
     <tr>
       <td>{props.ticker}</td>
-      <td>{finData.close}</td>
+      <td>{finData.price}</td>
       <td>{finData.date}</td>
-      <td>{finData.label}</td>
+      <td>{finData.time}</td>
     </tr>
   );
 };
