50e4008a57d672add3f714938e7549173545e20e
allezz <aerbay@gmail.com>
2020-09-09 20:50:09 +0200

    Add classnames package, Eject bootstrap css, Refactor all classNames

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React from "react";
 import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
+import globalStyles from "./stylesheets/global-styles/bootstrap.module.css";
+import cx from "classnames";
 import StockRow from "./components/StockRow.js";
+import styles from "./stylesheets/App.module.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <table className="table mt-9">
-          <thead>
-            <tr>
-              <th>Ticker</th>
-              <th>Price</th>
-              <th>Date</th>
-              <th>Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            <StockRow ticker="aapl" />
-            <StockRow ticker="goog" />
-            <StockRow ticker="msft" />
-            <StockRow ticker="tsla" />
-          </tbody>
-        </table>
+      <div className={cx(globalStyles.container)}>
+        <div className={cx(globalStyles["col-md-5"], globalStyles["mt-5"])}>
+          <div className={cx(globalStyles.card)}>
+            <ul
+              className={cx(
+                globalStyles["list-group"],
+                globalStyles["list-group-flush"]
+              )}
+            >
+              <li
+                className={cx(globalStyles["list-group-item"], styles.change)}
+              >
+                AAPL
+              </li>
+              <li className={cx(globalStyles["list-group-item"])}>AAPL</li>
+              <li className={cx(globalStyles["list-group-item"])}>AAPL</li>
+              <li className={cx(globalStyles["list-group-item"])}>AAPL</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
